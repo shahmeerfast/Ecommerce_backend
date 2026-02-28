ff5ba93340f4dd7a32c90cb6066ebbe2340d34a3
shahmeer631 <shahmeerkhokhar365@gmail.com>
2026-02-28 15:13:26 +0500

    mongo updated

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/e-commerce';
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/e-commerce';
 
     const conn = await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
